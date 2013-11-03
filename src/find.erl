23bcacf2c4167d891f205cfd87712db779cb719a
M. Stellmann <matthias.stellmann@haw-hamburg.de>
2013-11-03 12:36:26 +0100

    project structure created

--- src/find.erl
+++ src/find.erl
@@ -2,7 +2,7 @@
 %%% @author Matthias Stellmann and Grzegorz Markiewicz
 %%% @copyright (C) 2013, HAW Hamburg
 %%% @doc
-%%%
+%%%   TODO writing the doc
 %%% @private
 %%% @end
 %%% Created : 03. Nov 2013
