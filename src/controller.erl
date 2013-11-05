1a508b796cee232ece0caa5e8e36e5b6a3f642ec
Matthias Stellmann <matthias.stellmann@haw-hamburg.de>
2013-11-05 14:11:13 +0100

    wakeup function

--- src/controller.erl
+++ src/controller.erl
@@ -10,5 +10,22 @@
 -module(controller).
 -author("StellmannMarkiewicz").
 
+
+main(OwnNodeState, OwnLevel, OwnFragName, OwnEdgeList, OwnNodeName) ->
+  receive
+    wakeup when OwnNodeState == sleeping -> sleeping:wakeup(OwnEdgeList, OwnNodeName),
+      main(found, OwnLevel, OwnFragName, OwnEdgeList, OwnNodeName);
+
+    {initiate, Level, FragName, NodeState, Edge} -> doSomething;
+    {test, Level, FragName, Edge} -> doSomething;
+    {accept, Edge} -> doSomething;
+    {reject, Edge} -> doSomething;
+    {report, Weight, Edge} -> doSomething;
+    {changeroot, Edge} -> doSomething;
+    {connect, Level, Edge} -> doSomething
+  end
+
+.
+
 %% API
 -export([]).
--- src/sleeping.erl
+++ src/sleeping.erl
@@ -12,3 +12,26 @@
 
 %% API
 -export([]).
+
+
+%% @doc
+%% This function is called from the sleeping state of the node.
+%% It searches for the akmg and sends the connect msg to the node
+%% on the opposite side of the akmg. Afterwards the node changes into
+%% the sleeping state and waits for a response from the other node.
+%%    EdgeList:     Orddict where the key is the EdgeWeight and value
+%%                  is a tuple {OtherNodeName,EdgeState}
+%%    OwnNodeName:  The system known name of our own node
+wakeup(EdgeList, OwnNodeName)->
+  EdgeWeights = orddict:fetch_keys(EdgeList),
+    Akmg = lists:min(EdgeWeights),
+  % Value for the key (EdgeNumber) that is a Tuple {OtherNodeName, EdgeState}
+   EdgeToMark = orddict:fetch(Akmg, EdgeList),
+   OtherNodeName = element(1,EdgeToMark),
+   MarkedEdge = {OtherNodeName,branch},
+   NewEdgeList = orddict:store(Akmg,MarkedEdge,EdgeList),
+   OtherNodeName ! {connect, 0, {Akmg,OwnNodeName,OtherNodeName}},
+   ok
+.
+
+
