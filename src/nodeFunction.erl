cec8f45fd1909c52b596c72b839c8509d5df57d8
M. Stellmann <matthias.stellmann@haw-hamburg.de>
2013-11-06 16:20:07 +0100

    greg* super merge

--- src/nodeFunction.erl
+++ src/nodeFunction.erl
@@ -70,11 +70,6 @@ report(TestEdge, FindCount, OwnNodeState, InBranch, BestWT) ->
       {ok, TestEdge, found}
   end
 .
-%% @doc
-%%  TODO
-reject(Edge, OwnEdgeOrddict, OwnLevel, OwnNodeState, OwnFragName, FindCount, InBranch, BestWT) ->
-
-  .
 
 %% @doc
 %%  TODO
